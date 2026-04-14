8653ea533b6e67a915b69b1e0ed75da752d295ef
Isaac-Biotech <isaac.shepley0@gmail.com>
2026-04-14 17:51:23 -0600

    Update mission12.py

--- mission12.py
+++ mission12.py
@@ -1,12 +1,7 @@
 
 from Bio.Seq import Seq
-
 my_dna = Seq("ATGTTTGTTTATCGATAG")
-
-
 my_protein = my_dna.translate()
-
-
 print("----- Biopython Translator -----")
 print("Original DNA:", my_dna)
 print("Protein Chain:", my_protein)
