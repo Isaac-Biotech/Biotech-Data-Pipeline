73496c742dbe8d46f6216a41d9c9ea69ae1691be
Isaac-Biotech <isaac.shepley0@gmail.com>
2026-04-14 17:50:39 -0600

    Update mission13.py

--- mission13.py
+++ mission13.py
@@ -1,11 +1,6 @@
-#12 and 13 were made for homework
+
 from Bio.Seq import Seq
 from Bio.SeqUtils import gc_fraction
-
-# 1. Create our DNA sequence
 my_dna = Seq("ATGCGCGCGTATCGATAG")
-
-
 gc_percentage = gc_fraction(my_dna) * 100
-
 print(f"Professional GC Content: {gc_percentage:.2f}%")
